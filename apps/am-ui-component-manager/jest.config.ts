cc564e5d9fd2415101886b3a88d14a119972e335
Vladimir <vladimirdbalan@gmail.com>
2025-01-20 18:04:51 +0200

    Fixed base apps

---
+++ apps/am-ui-component-manager/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'am-ui-component-manager',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/am-ui-component-manager',
+};
---
+++ apps/am-ui-service-manager/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'am-ui-service-manager',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/am-ui-service-manager',
+};
